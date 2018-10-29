7503ae8aab54991c7d82eabc8084ef5e0baa9623
lukabuz <luka.buzaladze@mail.utoronto.ca>
2018-10-29 03:05:58 -0500

    added question checking when voting

--- app/Http/Controllers/PollController.php
+++ app/Http/Controllers/PollController.php
@@ -32,8 +32,8 @@ class PollController extends Controller
     public function vote(Request $request, $id){
         $poll = Poll::findOrFail($id);
 
-        if($poll->password !== null){
-            if(!Hash::check($request->input('password'), $poll->password)){
+        if ($poll->password !== null) {
+            if (!Hash::check($request->input('password'), $poll->password)) {
                 return $this->returnError('შეყვანილი პაროლი არასწორია!');
             }
         }
@@ -44,26 +44,26 @@ class PollController extends Controller
         //     return $this->returnError('გთხოვთ დაამტკიცოთ, რომ არ ხართ რობოტი');
         // }
 
-        if(!$request->exists('candidateId') || $request->input('candidateId') == ''){
+        if (!$request->exists('candidateId') || $request->input('candidateId') == '') {
             return $this->returnError('გთხოვთ აირჩიოთ კანდიდატი!');
         }
 
         $candidateId = $request->input('candidateId');
 
-        if(Candidate::where('poll_id', $poll->id)->where('id', $candidateId)->count() == 0){
+        if (Candidate::where('poll_id', $poll->id)->where('id', $candidateId)->count() == 0) {
             return $this->returnError('გთხოვთ აირჩიოთ ამ გამოკითხვის შესაბამისი კანდიდატი!');
         }
 
-        if($poll->requirePhoneAuth == 'True'){
+        if ($poll->requirePhoneAuth == 'True') {
             //check if phone # is valid
             $toMatch = '#^[+][1-9]{1}[0-9]{3,14}#';
-            if(!preg_match($toMatch , $number)) {
+            if (!preg_match($toMatch, $number)) {
                 return $this->returnError('გთხოვთ შეიყვანოთ სწორი ნომერი!');
             }
             
             //check if the number has been used before(compare hash to database hashes)
-            foreach(Vote::where('status', 'verified')->where('poll_id', $poll->id)->get() as $vote){
-                if(Hash::check($number, $vote->number)){
+            foreach (Vote::where('status', 'verified')->where('poll_id', $poll->id)->get() as $vote) {
+                if (Hash::check($number, $vote->number)) {
                     return $this->returnError('ეს ნომერი ერთხელ უკვე გამოყენებულია!');
                 };
             }
@@ -71,7 +71,7 @@ class PollController extends Controller
 
         $vote = new Vote;
         //create pin and store it in memory for the time being.
-        //this value is only kept within this function and 
+        //this value is only kept within this function and
         //is stored in the database as a hash
         $pin = rand(10000, 99999);
 
@@ -81,7 +81,7 @@ class PollController extends Controller
         $vote->status = 'unverified';
 
         //number and pin hashing
-        if($poll->requirePhoneAuth == 'False'){
+        if ($poll->requirePhoneAuth == 'False') {
             $vote->number = '';
             $vote->pin = '';
             $vote->status = 'verified';
@@ -90,13 +90,13 @@ class PollController extends Controller
             $vote->pin = Hash::make($pin);
 
             $res = $this->sendMessage($number, 'გამარჯობა! თქვენი Pollitic-ის ვერიფიკაციის კოდი არის: ' . $pin);
-            if(!$res){
+            if (!$res) {
                 return $this->returnError('მესიჯის გაგზავნისას დაფიქსირდა შეცდომა.');
             }
         }
 
         $res = $this->sendMessage($number, 'გამარჯობა! თქვენი Pollitic-ის ვერიფიკაციის კოდი არის: ' . $pin);
-        if(!$res){
+        if (!$res) {
             return $this->returnError('მესიჯის გაგზავნისას დაფიქსირდა შეცდომა.');
         }
 
@@ -104,12 +104,14 @@ class PollController extends Controller
 
         $vote->save();
 
-        foreach($request->questions as $question){
-            $answer = new PollQuestionAnswer;
-            $answer->vote_id = $vote['id'];
-            $answer->poll_question_id = $question['id'];
-            $answer->answer = $question['answer'];
-            $answer->save();
+        if ($request->exists('questions')) {
+            foreach ($request->questions as $question) {
+                $answer = new PollQuestionAnswer;
+                $answer->vote_id = $vote['id'];
+                $answer->poll_question_id = $question['id'];
+                $answer->answer = $question['answer'];
+                $answer->save();
+            }
         }
 
         return response()->json([
