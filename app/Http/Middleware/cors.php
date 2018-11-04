230d16b0eada09d1df899e5a17f6dbf3d651ee7d
lukabuz <luka.buzaladze@mail.utoronto.ca>
2018-11-04 16:30:24 -0500

    expanded CORS rules

--- app/Http/Middleware/cors.php
+++ app/Http/Middleware/cors.php
@@ -17,7 +17,8 @@ class Cors
     {
         return $next($request)
         ->header('Access-Control-Allow-Origin', '*')
-        ->header('Access-Control-Allow-Headers', 'X-PINGOTHER, Content-Type, Authorization, Content-Length, X-Requested-With')
+        ->header('Access-Control-Allow-Headers', 'X-PINGOTHER, Content-Type, Authorization, Content-Length, X-Requested-With, crossDomain')
+        ->header('Access-Control-Expose-Headers', 'Access-Control-Allow-Origin')
         ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
     }
 }
