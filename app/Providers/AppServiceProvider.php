866e039c4c0479fc5fe47aa78ad853174d1f8c77
lukabuz <luka.buzaladze@mail.utoronto.ca>
2018-11-09 15:26:02 -0500

    added forced https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
@@ -12,9 +13,10 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
         //
+        $url->forceSchema('https');
         Schema::defaultStringLength(191);
     }
 
